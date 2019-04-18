b0224847c566747e3b67e8a6558baef07f19f57d
Muhammad Aswan Syahputra <muhammadaswansyahputra@gmail.com>
2019-04-18 18:17:47 +0700

    fixed colnames of location

--- 006_kamisdata_indonesia-database-for-policy-and-economic-research/data-raw/indodapoer.R
+++ 006_kamisdata_indonesia-database-for-policy-and-economic-research/data-raw/indodapoer.R
@@ -14,10 +14,12 @@ glimpse(indodapoer_raw)
 
 indodapoer <- 
   indodapoer_raw %>% 
-  select(province = country_name, indicator = indicator_name, starts_with("x"), -x46) %>% 
+  select(province_or_district = country_name, indicator = indicator_name, starts_with("x"), -x46) %>% 
   gather(key = "year", value = "value", x1976:x2016) %>% 
   spread(indicator, value) %>% 
   clean_names() %>% 
   mutate(year = str_remove(year, "^x"))
+
+glimpse(indodapoer)
 
 save(indodapoer, file = "006_kamisdata_indonesia-database-for-policy-and-economic-research/data/indodapoer.rda", compress = "bzip2", compression_level = 9)
